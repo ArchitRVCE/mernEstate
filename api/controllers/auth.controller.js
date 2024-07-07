cdc7e14e8de90e47369e5d59c244ff0c47c91215
Archit <architchaturvedi2000@gmail.com>
2024-07-07 13:17:19 +0530

    sign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
 import { errorHandler } from "../utils/errorHandler.js";
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req,res,next) => {
     const {username,email,password} = req.body;
@@ -16,5 +17,21 @@ export const signUp = async (req,res,next) => {
         next(error);
         //next(errorHandler(420,"Testing errorhandler"))
     })
+
+}
+
+export const signIn = async (req,res,next) => {
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser)  return next(errorHandler(404,"User not found!"))
+        const validPassword = bcrypt.compareSync(password,validUser.password);
+        if(!validPassword) return next(errorHandler(401,"Incorrect Credentials"))
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+        const {password:extractPass,...rest} = validUser._doc;
+        res.cookie('access_token',token,{httpOnly:true, expiresIn: new Date(Date.now) + 24*60}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
 
 }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import {signUp} from '../controllers/auth.controller.js'
+import {signUp,signIn} from '../controllers/auth.controller.js'
 
 const route = express.Router()
 
 route.post("/signup",signUp);
-
+route.post("/signin",signIn);
 export default route
 
