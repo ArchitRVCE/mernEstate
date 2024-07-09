76c868c9986e8bb2c4c0b88a2a03525abf025a78
Archit <architchaturvedi2000@gmail.com>
2024-07-09 23:13:52 +0530

    update functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -5,12 +5,13 @@ import { updateFailed,updateStart,updateSuccess } from '../features/User/UserSli
 import { useSelector,useDispatch } from 'react-redux'
 
 function Profile() {
-  const {currentUser} = useSelector(state=>state.user);
+  const {currentUser,loading,error} = useSelector(state=>state.user);
   const fileRef = useRef(null);
   const [file,setFile] = useState(undefined);
   const [filePrec,setFilePerc] = useState(0);
   const [fileUploadError,setFileUploadError] = useState(false);
   const [formData,setFormData] = useState({});
+  const [updateSuccess,setupdateSucccess] = useState(false);
   const dispatch = useDispatch()
   useEffect(()=>{
     if(file)  handleFileUpload(file);
@@ -58,6 +59,7 @@ function Profile() {
         return;
       }
       dispatch(updateSuccess(data));
+      setupdateSucccess(true)
     } catch (error) {
         dispatch(updateFailed(error.message))
     }
@@ -85,12 +87,14 @@ function Profile() {
         <input type="text" placeholder='Username' id="username" defaultValue={currentUser.username} className='border p-3 rounder-lg' onChange={handleFormData}/>
         <input type="email" placeholder='Email' id="email" defaultValue={currentUser.email} className='border p-3 rounder-lg' onChange={handleFormData}/>
         <input type="password" placeholder='Password' id="password" className='border p-3 rounder-lg' onChange={handleFormData}/>
-        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-80'>Update</button>
+        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-80 disabled:opacity-80'>{loading?'Loading...':'Update'}</button>
       </form>
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
+      <p className='text-red-600 mt-5 text-center'>{error?error:''}</p>
+      <p className='text-green-600 mt-5 text-center'>{updateSuccess?'Updated Succefully':''}</p>
     </div>
   )
 }
