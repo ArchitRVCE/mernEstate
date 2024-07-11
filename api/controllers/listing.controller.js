5f9b493b53216b0076d8bd06053c3394869af9ab
Archit <architchaturvedi2000@gmail.com>
2024-07-11 14:28:35 +0530

    api route for getting listings

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/errorHandler.js";
 
 export const createListing = async(req,res,next) =>{
     try {
@@ -6,5 +7,15 @@ export const createListing = async(req,res,next) =>{
         res.status(201).json(listing);
     } catch (error) {
         next(error);
+    }
+}
+
+export const getUserListing = async(req,res,next) =>{
+    try {
+        if(req.user.id !== req.params.id) return next(errorHandler(403,'You can access your listings only'));
+        const data = await Listing.find({userRef:req.params.id});
+        res.status(200).json(data);
+    } catch (error) {
+        next(error)
     }
 }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing,getUserListing } from '../controllers/listing.controller.js';
 import { verifyUsers } from '../utils/verifyUsers.js';
 
 const router = express.Router();
 
 router.post("/create",verifyUsers,createListing)
-
+router.get('/listings/:id',verifyUsers,getUserListing)
 export default router
