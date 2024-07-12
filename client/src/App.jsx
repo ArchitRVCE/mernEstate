ecb6a937d4a36103f62477c50b700119ce29f13e
Archit <architchaturvedi2000@gmail.com>
2024-07-12 16:49:57 +0530

    add UI to search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './Components/PrivateRoute'
 import CreateListing from './Pages/CreateListing'
 import UpdateListing from './Pages/UpdateListing'
 import Listing from './Pages/Listing'
+import Search from './Pages/Search'
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           <Route path='/sign-in' element={<SignIn/>}/>
           <Route path='/sign-up' element={<SignUp/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path="/search" element={<Search/>}/>
           <Route element={<PrivateRoute/>}>
               <Route path='/profile' element={<Profile/>}/>
               <Route path='/create-listing' element={<CreateListing/>}></Route>
